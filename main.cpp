1868d54ee0036cfb7262207f1b1684976d85706d
FrancescoBerisso <115414198+FrancescoBerisso@users.noreply.github.com>
2022-12-07 18:19:34 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 
 int main(){
+  char c;
+  cout << "inserisci un carattere";
+  cin >> c;
+  if (carattere(c)) {
+      cout << c;
+  }
+  else {
+      cout << "errore";
+  }
   return 0;
 }
